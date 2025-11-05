9e6e1523279323614a233c51d0d73986ffb5e497
Matthew Hill <GeistInDerSH@gmail.com>
2025-11-05 20:05:50 +0100

    Update Script Regex to Match Features Changes
    Previously the features were updated to have the prefix 'solve_', however they can also have digits. The regex should include them when building/testing/cleaning
    
    Signed-off-by: Matthew Hill <GeistInDerSH@gmail.com>

--- rust/scripts/clean-feature-binaries.sh
+++ rust/scripts/clean-feature-binaries.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FEATURES=$(sed -nE 's/(solve_[a-zA-Z_]*) ?= ?\[.*\]/\1/p' Cargo.toml)
+FEATURES=$(sed -nE 's/(solve_[a-zA-Z_0-9]*) ?= ?\[.*\]/\1/p' Cargo.toml)
 
 echo "[INFO] Removing:"
 for feature in $FEATURES; do
