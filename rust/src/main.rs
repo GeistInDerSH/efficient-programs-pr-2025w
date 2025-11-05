237ffc20d725e1591f2f73d81504541ef16e1f16
Matthew Hill <geistindersh@gmail.com>
2025-11-05 10:00:41 +0100

    Add measure_runtime Feature
    Add a feature that will run the binary with runtime data reported. It isn't enabled by default, to allow for GNU time or other utils for measuring time with the OS overhead
    
    Signed-off-by: Matthew Hill <geistindersh@gmail.com>

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,16 +1,29 @@
-use std::time::Instant;
 use sudoku_solver::SudokuSolver;
 
 fn main() -> std::io::Result<()> {
-    let (solution, duration) = sudoku_solver::time_it!({
+    #[cfg(feature = "measure_runtime")]
+    {
+        use std::time::Instant;
+        let (solution, duration) = sudoku_solver::time_it!({
+            let file_path = std::env::args().nth(1).expect("No sudoku file specified");
+            let board = sudoku_solver::read_file(&file_path)?;
+            board.solve()
+        });
+        let time_micros = duration.as_micros();
+        match solution {
+            None => println!("No solution found. Took {time_micros}μs"),
+            Some(solution) => println!("Solution:\n{solution}\nTook {time_micros}μs"),
+        }
+    }
+    #[cfg(not(feature = "measure_runtime"))]
+    {
         let file_path = std::env::args().nth(1).expect("No sudoku file specified");
         let board = sudoku_solver::read_file(&file_path)?;
-        board.solve()
-    });
-    let time_micros = duration.as_micros();
-    match solution {
-        None => println!("No solution found. Took {time_micros}μs"),
-        Some(solution) => println!("Solution:\n{solution}\nTook {time_micros}μs"),
+        let solution = board.solve();
+        match solution {
+            None => println!("No solution found."),
+            Some(solution) => println!("Solution:\n{solution}"),
+        }
     }
     Ok(())
 }
