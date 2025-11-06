b1a1a9475c5c75d7d4493b9f60e033391752bea1
Matthew Hill <geistindersh@gmail.com>
2025-11-06 10:08:59 +0100

    Segment Benchmark Functions
    Break up the benchmarking functions so they can be selectively run for faster results
    
    Signed-off-by: Matthew Hill <GeistInDerSH@gmail.com>

--- rust/benches/benchmarks.rs
+++ rust/benches/benchmarks.rs
@@ -3,13 +3,31 @@ extern crate criterion;
 use criterion::Criterion;
 use sudoku_solver::SudokuSolver;
 
-fn criterion_benchmark(c: &mut Criterion) {
+fn file_io(c: &mut Criterion) {
     c.bench_function("read_file exists", |b| {
         b.iter(|| sudoku_solver::read_file("../boards/example-1.sudoku"))
     });
     c.bench_function("read_file dne", |b| {
         b.iter(|| sudoku_solver::read_file("invalid/file.sudoku"))
     });
+}
+
+fn invalid_boards(c: &mut Criterion) {
+    c.bench_function("solve for invalid board (multiple collisions)", |b| {
+        b.iter(|| sudoku_solver::example_boards::INVALID_BOARD_ROW_AND_COL_COLLISION.solve());
+    });
+    c.bench_function("solve for invalid board (row collisions)", |b| {
+        b.iter(|| sudoku_solver::example_boards::INVALID_BOARD_ROW_COLLISION.solve());
+    });
+    c.bench_function("solve for invalid board (col collisions)", |b| {
+        b.iter(|| sudoku_solver::example_boards::INVALID_BOARD_COL_COLLISION.solve());
+    });
+    c.bench_function("solve for invalid board (box collisions)", |b| {
+        b.iter(|| sudoku_solver::example_boards::INVALID_BOARD_BOX_COLLISION.solve());
+    });
+}
+
+fn solvable_boards(c: &mut Criterion) {
     c.bench_function("solve for valid board (easy)", |b| {
         b.iter(|| sudoku_solver::example_boards::EASY_BOARD.solve());
     });
@@ -28,10 +46,8 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("solve for valid board (solved)", |b| {
         b.iter(|| sudoku_solver::example_boards::SOLVED_BOARD.solve());
     });
-    c.bench_function("solve for valid board (invalid)", |b| {
-        b.iter(|| sudoku_solver::example_boards::INVALID_BOARD_ROW_AND_COL_COLLISION.solve());
-    });
 }
 
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
+criterion_group!(io, file_io);
+criterion_group!(boards, solvable_boards, invalid_boards);
+criterion_main!(io, boards);
