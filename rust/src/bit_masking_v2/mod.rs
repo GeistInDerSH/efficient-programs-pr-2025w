e8c595facead4d0ac487bf8507b74666f5c0b3ff
Matthew Hill <GeistInDerSH@gmail.com>
2025-11-11 14:08:30 +0100

    Code Cleanup
    The bit-and and bit-or operations can be inlined into the if statement to make it more readable. This doesn't impact performance
    
    Signed-off-by: Matthew Hill <GeistInDerSH@gmail.com>

--- rust/src/bit_masking_v2/mod.rs
+++ rust/src/bit_masking_v2/mod.rs
@@ -28,14 +28,8 @@ fn solve(
         for p in 1..=9 {
             let mask = 1 << p;
 
-            let any_seen = {
-                let mut seen = 0;
-                seen |= row_bits[row] & mask;
-                seen |= col_bits[col] & mask;
-                seen |= box_bits[box_number] & mask;
-                seen
-            };
-            if any_seen != 0 {
+            if (row_bits[row] & mask) | (col_bits[col] & mask) | (box_bits[box_number] & mask) != 0
+            {
                 continue;
             }
 
@@ -78,14 +72,7 @@ impl super::SudokuSolver for Board {
 
                 let box_number = row.div(3) * 3 + col.div(3);
                 let mask = 1 << value;
-                let any_seen = {
-                    let mut seen = 0;
-                    seen |= rows[row] & mask;
-                    seen |= cols[col] & mask;
-                    seen |= boxes[box_number] & mask;
-                    seen
-                };
-                if any_seen != 0 {
+                if (rows[row] & mask) | (cols[col] & mask) | (boxes[box_number] & mask) != 0 {
                     return None;
                 }
 
